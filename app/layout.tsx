b017567ad7bf87adb388da98bd7675d1d4c21f97
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-07-06 22:54:45 +0900

    feat: font 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 'use client'
 
+import { Roboto } from 'next/font/google'
 import './globals.css'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>{children}</body>
     </html>
   )
