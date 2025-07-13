8a2cce1282932b77ec8a23e6589c9f10b3ec2e55
icw0201 <icw0207@naver.com>
2025-07-13 14:35:39 +0900

    [refactor] api 경로 수정, 파일명 변경 - api 경로명 변경 - 파일 명 register -> create로 통일

--- backend/application/admin/boardgames/dtos/CreateBoardgameDto.ts
+++ backend/application/admin/boardgames/dtos/CreateBoardgameDto.ts
@@ -1,6 +1,6 @@
-// backend/application/admin/boardgames/dtos/RegisterBoardgameDto.ts
+// backend/application/admin/boardgames/dtos/CreateBoardgameDto.ts
 
-export interface RegisterBoardgameDto {
+export interface CreateBoardgameDto {
   userId: string // created_by, updated_by 에 사용될 사용자 ID (UUID)
   korName: string // name 컬럼에 매핑될 한국어 이름
   genreId: number
