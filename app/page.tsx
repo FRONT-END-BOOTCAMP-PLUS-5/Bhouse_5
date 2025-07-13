7122d7a45fa73fb663b8b64de8a8d9676e0ae420
HBDCHAEYOUNG <devyoon02@gmail.com>
2025-07-14 00:29:06 +0900

    [feat]api 로직 정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className={styles.page}>
-      <Link href="/home">Home</Link>
+      <Link href="/auth/signin">Home</Link>
       <div>Bhouse_5</div>
     </div>
   )
