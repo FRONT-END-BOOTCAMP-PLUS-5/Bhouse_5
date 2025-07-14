c7847f260da4e43d4e67d885293c990847293b8b
icw0201 <icw0207@naver.com>
2025-07-14 11:45:43 +0900

    [style] 모바일 폰트 스타일 입력

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import React from 'react'
 
 const roboto = Roboto({
   weight: '400',
@@ -11,6 +12,9 @@ const roboto = Roboto({
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={roboto.className}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   )
