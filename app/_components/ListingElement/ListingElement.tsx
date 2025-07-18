9db4a54bb8d4047e13cc0ddba1305820b50a076c
icw0201 <icw0207@naver.com>
2025-07-18 10:46:14 +0900

    [refactor][fix] CircleButton component 변경 적용, Boardgame entity 원복 (#44)
    * [feat] BE-Alarm API 수정/추가
    - alarm 응답값으로 post_id 추가하도록 변경
    - alarm '읽음 처리' api 추가
    
    * [feat] header 수정 중 WIP
    
    * [feat] BE-Alarm API 수정/추가
    - alarm 응답값으로 post_id 추가하도록 변경
    - alarm '읽음 처리' api 추가
    
    * [feat] header 수정 중 WIP
    
    * [feat] add Toggle component
    
    * [feat] header alarm service 추가
    
    * [refactor] header 간격 조정
    
    * [chore] WIP
    
    * [fix] 예시 이미지 도메인 추가
    - example.com 주소의 프로필 img url을 가진 사용자가 로그인해도
    헤더가 오류 없이 랜더링되도록 수정
    
    * [chore] 이미지 사이즈 변경
    
    * [refactor]헤더 네비게이션, 로그아웃 api 수정 @HBDCHAEYOUNG
    
    * [refactor] 헤더 컴포넌트 처리 방식 변경
    - 컴포넌트 내부에서 페이지 이동 처리하도록 수정
    - 컴포넌트 버튼 눌렀을 때 닫히도록 수정 등
    
    * [refactor] CircleButton refactor #39
    
    * [chore] PR 전 변경 저장
    
    * [refactor] CircleButton 사용 수정
    - 컴포넌트 정의 변경에 따라, 컴포넌트 사용하는 페이지의 코드 수정
    
    ---------
    
    Co-authored-by: HBDCHAEYOUNG <devyoon02@gmail.com>

--- app/_components/ListingElement/ListingElement.tsx
+++ app/_components/ListingElement/ListingElement.tsx
@@ -3,22 +3,24 @@
 import React from 'react'
 import styles from './ListingElement.module.css'
 import CircleButton from '../CircleButton/CircleButton'
-import FileIcon from '@public/icons/trash.svg'
+import TrashIcon from '@public/icons/trash.svg'
 
 interface ListingElementProps {
   label: string
   onDelete: () => void
 }
-//TODO:button circle로 바꾸기
 
 const ListingElement: React.FC<ListingElementProps> = ({ label, onDelete }) => {
   return (
     <div className={styles.boardgameItem}>
       <div className={styles.boardgameLabel}>{label}</div>
       <CircleButton
-        icon={<FileIcon width={20} height={20} fill="black" />} // iconSize 대신 width, height를 직접 지정합니다. 상단에서 import한 아이콘을 가져옵니다.
-        //사용할 svg아이콘의 파일에서 fill값을 "currentColor"로 지정해야 색상을 바꿀 수 있습니다. (예시 파일 : bell, file, trash 수정하였음.)
-        iconAlt="파일" // iconAlt는 CircleButton 인터페이스에는 있지만, FileIcon 컴포넌트 자체에서 alt를 관리할 수도 있습니다.
+        svgComponent={TrashIcon} // BellIcon 컴포넌트를 직접 전달
+        svgWidth={20} // 아이콘 너비
+        svgHeight={20} // 아이콘 높이
+        svgFill="black" // 아이콘 색상
+        imageAlt="삭제 아이콘" // 접근성용 대체 텍스트
+        bgColor="var(--secondary-blue-200)"
         size={40}
         onClick={() => alert('파일 아이콘 클릭')} // onClick 메시지를 더 구체적으로 변경했습니다.
       />
