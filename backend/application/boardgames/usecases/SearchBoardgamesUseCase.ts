63522d0202305079247a20020d0fb7f0813d5da9
icw0201 <icw0207@naver.com>
2025-07-19 18:17:04 +0900

    [feat] boardgame API 수정 - like_count 받아오기

--- backend/application/boardgames/usecases/SearchBoardgamesUseCase.ts
+++ backend/application/boardgames/usecases/SearchBoardgamesUseCase.ts
@@ -3,12 +3,13 @@
 import { BoardgameRepository } from '@domain/repositories/BoardgameRepository'
 import { SearchBoardgamesDto } from '../dtos/SearchBoardgamesDto'
 import { BoardGameResponseDto } from '../dtos/BoardGameResponseDto'
+import { Boardgame } from '@be/domain/entities/Boardgame' // Boardgame 엔티티 임포트
 
 export class SearchBoardgamesUseCase {
   constructor(private readonly repo: BoardgameRepository) {}
 
   async execute(params: SearchBoardgamesDto): Promise<BoardGameResponseDto[]> {
-    const boardgames = await this.repo.searchBoardgames(params)
+    const boardgames = (await this.repo.searchBoardgames(params)) as (Boardgame & { likeCount: number })[] // likeCount 속성 추가된 타입으로 캐스팅
 
     return boardgames.map((game) => ({
       id: game.boardgameId,
@@ -16,6 +17,7 @@ export class SearchBoardgamesUseCase {
       min_players: game.minPlayers ?? 0,
       max_players: game.maxPlayers ?? 0,
       img_url: game.imgUrl ?? '',
+      like_count: game.likeCount, // repository에서 받아온 likeCount 사용
     }))
   }
 }
