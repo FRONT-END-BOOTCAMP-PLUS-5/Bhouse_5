d9301a8e8834d3ec3acab28755eb7b1481cd69c6
icw0201 <icw0207@naver.com>
2025-07-20 16:21:19 +0900

    [refactor] layout요소 link 수정

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -2,36 +2,17 @@
 'use client' // 클라이언트 컴포넌트로 지정
 
 import React from 'react'
+import Link from 'next/link' // Link 컴포넌트 임포트
 import styles from './Footer.module.css' // Footer 전용 CSS 모듈
 
 const Footer: React.FC = () => {
-  const handleCustomerSupportClick = () => {
-    console.log('고객지원 클릭')
-    // TODO: 고객지원 페이지로 이동 로직 추가
-  }
-
-  const handleContactUsClick = () => {
-    console.log('문의하기 클릭')
-    // TODO: 문의하기 페이지로 이동 로직 추가
-  }
-
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <a
-          href="#"
-          onClick={handleCustomerSupportClick}
-          className={`${styles.footerLink} ${styles.body16} ${styles.whiteText}`}
-        >
+        {/* Link 컴포넌트를 사용하여 /support 페이지로 이동 */}
+        <Link href="/support" className={`${styles.footerLink} ${styles.body16} ${styles.whiteText}`}>
           고객지원
-        </a>
-        <a
-          href="#"
-          onClick={handleContactUsClick}
-          className={`${styles.footerLink} ${styles.body16} ${styles.whiteText}`}
-        >
-          문의하기
-        </a>
+        </Link>
       </div>
     </footer>
   )
