283f3b8f6c9ae887fe446035473c3d28fc193fae
icw0201 <icw0207@naver.com>
2025-07-20 16:25:27 +0900

    [feat] 메인 배너 이미지 추가 및 적용

--- app/(anon)/home/page.tsx
+++ app/(anon)/home/page.tsx
@@ -2,19 +2,64 @@
 'use client' // 클라이언트 컴포넌트로 지정
 
 import React from 'react'
+import Carousel from '@/_components/Carousel/Carousel'
 import BoardgameList from './_components/BoardgameList' // BoardgameList 컴포넌트 경로
-import styles from '@/page.module.css' // app/page.module.css 임포트
+import PostList from './_components/PostList' // PostList 컴포넌트 경로
+import globalCss from '@/page.module.css' // app/page.module.css 임포트 (이름 변경)
+import styles from './homePage.module.css' // 새로 생성된 homePage.module.css 임포트
+import Image from 'next/image' // Image 컴포넌트를 사용하기 위해 임포트합니다.
 
 export default function TestBoardgamesPage() {
   return (
-    <main className={styles.page}>
-      {' '}
+    <main className={globalCss.page}>
       {/* app/page.module.css의 .page 스타일 적용 */}
-      <div className="container mx-auto p-4">
+      <div className={styles.container}>
         {' '}
-        {/* Tailwind CSS 클래스 사용 (예시) */}
-        <h1 className={styles.header20}>보드게임 목록 테스트 페이지</h1>
-        <BoardgameList /> {/* BoardgameList 컴포넌트 렌더링 */}
+        {/* Tailwind 클래스 대신 CSS 모듈 클래스 적용 */}
+        <Carousel
+          items={[
+            {
+              content: (
+                <Image
+                  src="/images/banner1.png"
+                  alt="Ad 1"
+                  width={612}
+                  height={408}
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              ),
+              href: '/store-register',
+            },
+            {
+              content: (
+                <Image
+                  src="/images/banner2.png"
+                  alt="Ad 3"
+                  width={800}
+                  height={300}
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              ),
+              href: '/boardgames', //FIXME : 보드게임 링크 구현후 수정
+            },
+            {
+              content: (
+                <Image
+                  src="/images/banner3.png"
+                  alt="Ad 3"
+                  width={800}
+                  height={300}
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+              ),
+              href: '/community/posts',
+            },
+          ]}
+          autoPlay
+          interval={10000}
+        />
+        {/* <BoardgameList /> */}
+        <PostList />
       </div>
     </main>
   )
